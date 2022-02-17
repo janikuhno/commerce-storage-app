2096143792a167197bee15bc8218bd88f596a51a
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-02-17 17:52:03 +0200

    simplified backend logic

--- database.sql
+++ database.sql
@@ -14,26 +14,28 @@ CREATE TABLE users(
 
 --products
 CREATE TABLE products(
-    product_id SERIAL,
-    code BIGINT NOT NULL UNIQUE, --example GTIN/EAN code: 6420256001547 (13 integers)
-    full_name VARCHAR(255) NOT NULL,
+    code BIGINT, --example GTIN/EAN code: 6420256001547 (13 integers)
+    name VARCHAR(255) NOT NULL,
     image_path VARCHAR(255),
     weight INT NOT NULL,
-    PRIMARY KEY (product_id)
+    kcal INT NOT NULL,
+    PRIMARY KEY (code)
 );
 
 --nutritions (in 100g)
+/*
 CREATE TABLE nutritions(
     nutrition_id SERIAL,
-    product_id SERIAL,
-    energy_kcal INT,
+    code BIGINT,
     energy_kj INT,
+    energy_kcal INT,
     fat DECIMAL,
     fat_saturated DECIMAL,
     carbs DECIMAL,
     carbs_sugar DECIMAL,
     protein DECIMAL,
     salt DECIMAL,
-    PRIMARY KEY (nutrition_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    PRIMARY KEY (nutrition_id, code),
+    FOREIGN KEY (code) REFERENCES products(code)
 );
+*/
--- routes/products.js
+++ routes/products.js
@@ -4,13 +4,65 @@ const pool = require('../db');
 
 router.post('/products', authorize, async (req, res) => {
   try {
-    const { code, name, image, weight } = req.body;
+    const { code, name, image, weight, kcal } = req.body;
     const newProduct = await pool.query(
-      `INSERT INTO products(code, full_name, image_path, weight) VALUES($1, $2, $3, $4) RETURNING *`,
-      [code, name, image, weight]
+      `INSERT INTO products(code, name, image_path, weight, kcal) VALUES($1, $2, $3, $4, $5) RETURNING *`,
+      [code, name, image, weight, kcal]
     );
 
     res.json(newProduct.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+router.get('/products', authorize, async (req, res) => {
+  try {
+    const { code } = req.body;
+    const product = await pool.query(
+      'SELECT code, name, weight, kcal FROM products WHERE code = $1',
+      [code]
+    );
+
+    res.json(product.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+router.put('/products/:code', authorize, async (req, res) => {
+  try {
+    const { code } = req.params;
+    const { name, image, weight, kcal } = req.body;
+    const updateProduct = await pool.query(
+      'UPDATE products SET name = $1, image_path = $2, weight = $3, kcal = $4 WHERE code = $5 RETURNING *',
+      [name, image, weight, kcal, code]
+    );
+
+    if (updateProduct.rows.length === 0) {
+      return res.json('Unauthorized!');
+    }
+
+    res.json('Product was updated');
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+router.delete('/products/:code', authorize, async (req, res) => {
+  try {
+    const { code } = req.params;
+    const deleteProduct = await pool.query(
+      'DELETE FROM products WHERE code = $1 RETURNING *',
+      [code]
+    );
+
+    if (deleteProduct.rows.length === 0) {
+      return res.json('Unauthorized!');
+    }
+
+    res.json('Product was deleted');
   } catch (err) {
     console.error(err.message);
   }
