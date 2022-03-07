220005968efffb857aeb5272d47d8df1f9f696a8
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-03-07 14:35:57 +0200

    create image display

---
+++ client/src/components/productList/ImageProduct.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const ImageProduct = ({ product }) => {
+  return (
+    <Fragment>
+      <div></div>
+    </Fragment>
+  );
+};
+
+export default ImageProduct;
