85034e1a8ea63faa81563bcf16606b9c32ce559f
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-03-09 17:09:01 +0200

    gets all products

--- client/src/components/productList/ListAllProduct.js
+++ client/src/components/productList/ListAllProduct.js
@@ -1,11 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Navigation from '../Navigation';
 
 const ListAllProduct = ({ setAuth }) => {
+  const [allProducts, setAllProducts] = useState([]);
+
+  const getAll = async () => {
+    try {
+      const response = await fetch('/allproducts/products', {
+        method: 'GET',
+        headers: { jwt_token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      setAllProducts(parseRes);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
   return (
     <Fragment>
       <Navigation setAuth={setAuth} />
-      <div>Hello!</div>
+      <table className="table mt-5">
+        <thead>
+          <tr>
+            <th>EAN/GTIN -koodi</th>
+            <th>Nimi</th>
+            <th>Kuvapolku</th>
+            <th>Paino</th>
+            <th>Kcal</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allProducts.length !== 0 &&
+            allProducts[0].code !== null &&
+            allProducts.map((p) => (
+              <tr key={p.code}>
+                <td>{p.code}</td>
+                <td>{p.name}</td>
+                <td>{p.image_path}</td>
+                <td>{p.weight}</td>
+                <td>{p.kcal}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </Fragment>
   );
 };
