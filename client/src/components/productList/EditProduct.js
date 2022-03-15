99cf1c29b3c98cd3b98b9ff5201b8b18b040ef88
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-03-15 18:22:36 +0200

    add edit

---
+++ client/src/components/productList/EditProduct.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const EditProduct = ({ product, allProducts, setAllProducts }) => {
+  return (
+    <Fragment>
+      <div></div>
+    </Fragment>
+  );
+};
+
+export default EditProduct;
