dbcd7ec88cdbaea373a853d8fc18e8981c85c22c
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-03-18 15:48:08 +0200

    add for future

---
+++ client/src/components/productList/SearchAllProducts.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+/*
+https://medium.com/geekculture/create-a-simple-search-component-in-react-js-using-react-hooks-710c1dfe8b58
+*/
+
+const SearchAllProducts = ({ allProducts, setAllProducts }) => {
+  return (
+    <Fragment>
+      <div></div>
+    </Fragment>
+  );
+};
+
+export default SearchAllProducts;
